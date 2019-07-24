55518541a35b61f8a0fe32ecc2d95ad854a434d1
adriancobon <49730380+adriancobon@users.noreply.github.com>
2019-07-24 16:40:20 -0400

    added data file

---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+const data = [
+    {
+        id: 1,
+        name: "Alex Trebeck",
+        email: "quizmaster@jeopardy.com",
+        phone: "1-800-qik-answ"
+    }
+
+    {
+        id: 2,
+        name: "Bob the Builder",
+        email: "buildwithme@builder.com",
+        phone: "1-800-hap-hose"
+    }
+]
